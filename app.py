455c0e6de6ec888b1d3b22afa2b9c79fad035820
unknown <luisfelipezamodzkicoelho@gmail.com>
2026-01-29 09:33:59 -0300

    Major updates

--- app.py
+++ app.py
@@ -1,65 +1,83 @@
 import streamlit as st
 import plotly.graph_objects as go
-from connection import API_KEY, url
+import plotly.express as px
+from services.connection import API_KEY, url
 import pandas as pd
 import requests
 from datetime import date
+from services.market_data import fetch_data, data_treatment, daily_data, multiple_normalized_data, multiple_volume_data
+from layout.charts import create_candlestick_graph, create_volume_line_graph, create_normalized_line_graph
 
-
-#Select stock symbol
-symbol = st.selectbox("Select Stock Symbol", options=["IBM", "AAPL", "GOOGL", "MSFT"], index=0)
-params = {
-    'function': 'TIME_SERIES_DAILY',
-    'symbol': symbol,
-    'interval': '5min',
-    'apikey': API_KEY
-}
-
-# Fetch data from API
-def fetch_data(url, params):
-    response = requests.get(url, params=params)
-    data = response.json()
-    return data
-
-# Function to process the data
-def data_treatment(data):
-    serie = data['Time Series (Daily)']
-    data_frame = pd.DataFrame.from_dict(serie, orient='index')
-    data_frame = data_frame.rename(columns={
-        '1. open': 'open',
-        '2. high': 'high',
-        '3. low': 'low',
-        '4. close': 'close',
-        '5. volume': 'volume'})
-    data_frame.index = pd.to_datetime(data_frame.index)
-    data_frame = data_frame.astype(float)
-    data_frame = data_frame.sort_index()
-    return data_frame
-
-# Caching the data to optimize performance
-@st.cache_data
-def daily_data(symbol, today):
-    return fetch_data(url, params)
-
-
-# Main Streamlit App
-st.title("Stock Data Visualization")
-graph = go.Figure(data=[
-    go.Candlestick(
-        x = data_treatment(daily_data(symbol, date.today())).index,
-        open = data_treatment(daily_data(symbol, date.today()))['open'],
-        high = data_treatment(daily_data(symbol, date.today()))['high'],
-        low = data_treatment(daily_data(symbol, date.today()))['low'],
-        close = data_treatment(daily_data(symbol, date.today()))['close']
-    )
-    ])
-graph.update_layout(
-    yaxis_title='Price (USD per Share)',
-    xaxis_title='Date'
+st.set_page_config(
+    page_title="Stock Market Data Visualization",
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
-st.header(f"{symbol} Daily Candlestick Chart")
-st.plotly_chart(graph, use_container_width=True)
+st.header("Stock Market Data Visualization")
+"""
+    Choose a stock symbol and chart types to visualize the stock market data.
+"""
+""
+collum1, collum2 = st.columns([1, 5])
 
-st.header(f"{symbol} Daily Trading Volume")
-st.line_chart(data_treatment(daily_data(symbol, date.today()))[['volume']])
+
+# Select Chart Types
+with collum1.container(border=True):
+    st.write("##### Select Chart Types") 
+    chart_types = st.multiselect("", ["Candlestick Chart", "Normalized Line Chart"])
+
+# Select Stock Symbol
+with collum1.container(border=True):
+    st.write("##### Select Stock Symbol")
+
+    options = ["AAPL", "IBM", "GOOGL", "MSFT", "NVDA", "AMZN", "TSLA", "META"]
+    selected = []
+    for option in options:
+        if st.checkbox(option):
+            selected.append(option)
+
+
+# Display Volume Chart
+with collum2.container(border=True):
+    st.subheader("Volume Over Time", text_alignment="center")
+    st.plotly_chart(create_volume_line_graph(multiple_volume_data(selected), selected))
+
+# Display Normalized Line Chart
+if "Normalized Line Chart" in chart_types:
+    with st.container(border=True):
+        if len(selected) > 1:
+            st.subheader("Normalized Line Chart", text_alignment="center")
+            st.plotly_chart(create_normalized_line_graph(multiple_normalized_data(selected), selected))
+        else:
+            st.warning("Please select at least two stock symbols for Normalized Line Chart.")
+
+
+collum3, collum4 = st.columns([1, 5])
+if "Candlestick Chart" in chart_types:
+    with collum3.container(border=True):
+        st.write("##### Select Stock Symbol for Candlestick Chart")
+        symbol = st.radio("", options=["AAPL", "IBM", "GOOGL", "MSFT", "NVDA", "AMZN", "TSLA", "META"])
+        params = {
+            'function': 'TIME_SERIES_DAILY',
+            'symbol': symbol,
+            'apikey': API_KEY
+        }
+    daily_data = daily_data(symbol, date.today(), params)
+    with collum3.container(border=True):
+        indicator_options = ["SMA20", "EMA20", "BOLLINGER BANDS"]
+        indicator_selected = []
+        st.write("##### Select Tecnical Indicator")
+        for indicator in indicator_options:
+            if st.checkbox(indicator):
+                indicator_selected.append(indicator)
+
+# Display Candlestick Chart
+    with collum4.container(border=True):
+        st.subheader(f"{symbol} Candlestick Chart", text_alignment="center")
+        st.plotly_chart(create_candlestick_graph(daily_data, indicator_selected))
+    # Display Raw Data
+    with st.expander("Raw_Data"):
+        st.write(data_treatment(daily_data))
+
+
 
